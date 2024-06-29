f289f010772dbad622324aaf704da2b7e6df69d1
Joel Sousa <joe_sousa@alu.ufc.br>
2024-06-29 20:48:46 -0300

    feat: Create Swagger to  document the Spring REST API.

---
+++ src/main/java/projeto/simulados/utils/OpenApiConfig.java
@@ -0,0 +1,10 @@
+package projeto.simulados.utils;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenApiConfig {
+    
+}
